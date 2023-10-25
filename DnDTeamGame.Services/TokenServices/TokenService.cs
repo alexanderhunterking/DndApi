7592b3d690f871d8693fc464d91487dad0fa0280
Alexander King <143135033+alexanderhunterking@users.noreply.github.com>
2023-10-25 12:13:51 -0400

    Update TokenService.cs

--- DnDTeamGame.Services/TokenServices/TokenService.cs
+++ DnDTeamGame.Services/TokenServices/TokenService.cs
@@ -57,6 +57,7 @@ namespace DnDTeamGame.Services.TokenServices
 
             TokenResponse response = new TokenResponse()
             {
+                UserId = entity.UserId
                 Token = tokenHandler.WriteToken(token),
                 IssuedAt = token.ValidFrom,
                 Expires = token.ValidTo
