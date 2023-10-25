d323f0a5fdc27678bf70787029987a0e90fa7ef6
Alexander King <143135033+alexanderhunterking@users.noreply.github.com>
2023-10-25 12:11:15 -0400

    User Prop added to token model

--- DnDTeamGame.Models/TokenModels/TokenResponse.cs
+++ DnDTeamGame.Models/TokenModels/TokenResponse.cs
@@ -2,6 +2,7 @@ namespace DnDTeamGame.Models.TokenModels;
 
 public class TokenResponse
 {
+    public in UserId {get; set;}
     public string Token { get; set; } = null!;
     public DateTime IssuedAt { get; set; }
     public DateTime Expires { get; set; }
