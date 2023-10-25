e715877c23230c17d9f15bb05fc33a88d94eb011
Alexander King <143135033+alexanderhunterking@users.noreply.github.com>
2023-10-25 12:38:57 -0400

    Update TokenResponse.cs

--- DnDTeamGame.Models/TokenModels/TokenResponse.cs
+++ DnDTeamGame.Models/TokenModels/TokenResponse.cs
@@ -2,7 +2,7 @@ namespace DnDTeamGame.Models.TokenModels;
 
 public class TokenResponse
 {
-    public in UserId {get; set;}
+    public int Id {get; set;}
     public string Token { get; set; } = null!;
     public DateTime IssuedAt { get; set; }
     public DateTime Expires { get; set; }
